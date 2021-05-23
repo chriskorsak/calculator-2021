61716420ad0403dde287ddf604e375a02f875d9f
chriskorsak <chris@chriskorsak.com>
2021-05-23 15:53:55 -0700

    create math operator functions, operator function.

---
+++ scripts.js
@@ -0,0 +1,19 @@
+// math operation functions
+const add = (num1, num2) => num1 + num2;
+const subtract = (num1, num2) => num1 - num2;
+const multiply = (num1, num2) => num1 * num2;
+const divide = (num1, num2) => num1 / num2;
+
+//operate function choses which math function to run
+function operate(num1, num2, operator) {
+  switch(operator) {
+    case '+':
+      return add(num1, num2);
+    case '-':
+      return subtract(num1, num2);
+    case '*':
+      return multiply(num1, num2);
+    case '/':
+      return divide(num1, num2);
+  }
+}
