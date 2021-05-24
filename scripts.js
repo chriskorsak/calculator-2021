25a4993df81a1868c3ded130f45498383e166a80
chriskorsak <chris@chriskorsak.com>
2021-05-23 21:13:09 -0700

    implement display numbers in calculator display readout: add dom variables, displayValue variable, number button event listeners, displayNumber function.

--- scripts.js
+++ scripts.js
@@ -1,3 +1,14 @@
+//GLOBALS
+let displayValue = '';
+
+//DOM Elements
+const displayDiv = document.querySelector('#display');
+const numbersButtons = document.querySelectorAll('.numbers');
+
+//EVENT LISTENERS
+numbersButtons.forEach(number => number.addEventListener('click', displayNumber));
+
+//FUNCTIONS
 // math operation functions
 const add = (num1, num2) => num1 + num2;
 const subtract = (num1, num2) => num1 - num2;
@@ -19,4 +30,14 @@ function operate(num1, num2, operator) {
     case '/':
       return divide(num1, num2);
   }
+}
+
+//function that shows the numbers pushed on calculater display readout
+function displayNumber(e) {
+  //get number value (as a string) from button push
+  const number = e.target.textContent;
+  //string concat at this point, not actual number yet
+  displayValue += number;
+  //update display readout
+  displayDiv.textContent = displayValue;
 }
