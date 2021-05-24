0ec71ddddaf5f31ec456b7dd3898e347aece0eff
chriskorsak <chris@chriskorsak.com>
2021-05-23 17:29:51 -0700

    update divide function with error message output if dividing by zero.

--- scripts.js
+++ scripts.js
@@ -2,7 +2,10 @@
 const add = (num1, num2) => num1 + num2;
 const subtract = (num1, num2) => num1 - num2;
 const multiply = (num1, num2) => num1 * num2;
-const divide = (num1, num2) => num1 / num2;
+const divide = (num1, num2) => {
+  return (num2 === 0) ? 'Error' 
+  : num1 / num2
+}
 
 //operate function choses which math function to run
 function operate(num1, num2, operator) {
